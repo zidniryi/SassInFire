640d0566ba10521935ca191ade72935f7db71732
zidniryi <zidnirydwan@gmail.com>
2019-08-20 22:46:43 +0700

    Custom Sass and Separte File

---
+++ js/auth.js
@@ -0,0 +1,33 @@
+function swapSignForms(bool) {
+    if (bool) {
+       document.querySelector('#register-container').classList.add('inactive-form');
+		 document.querySelector('#register-container').classList.remove('active-form');
+       setTimeout(() => {
+           document.querySelector('#register-container').style.display = "none";
+           document.querySelector('#login-container').style.display = "block";
+           document.querySelector('#login-container').classList.add('active-form');
+			  document.querySelector('#login-container').classList.remove('inactive-form');
+       }, 200);
+    } else {
+       document.querySelector('#login-container').classList.add('inactive-form');
+		 document.querySelector('#login-container').classList.remove('active-form');
+       setTimeout(() => {
+           document.querySelector('#login-container').style.display = "none";
+           document.querySelector('#register-container').style.display = "block";
+           document.querySelector('#register-container').classList.add('active-form');
+			  document.querySelector('#register-container').classList.remove('inactive-form');
+       }, 200);
+    }
+}
+
+var loginTransition = document.querySelector('#login-transition');
+var registerTransition = document.querySelector('#register-transition');
+
+loginTransition.addEventListener('click', function(){
+	swapSignForms(true);
+});
+registerTransition.addEventListener('click', function(){
+	swapSignForms(false);
+});
+
+swapSignForms(true);
